ceb669885da35879794a669594e882ca5ee447c3
awesomesauss <quanbiao.25@ichat.sp.edu.sg>
2026-05-06 09:19:05 +0800

    For Lab 3

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,5 @@
 import statistics as stats
+
 def display_main_menu():
    print("Hello, please enter some numbers seperated by commas! (e.g. 5, 67, 32)")
 
@@ -27,17 +28,17 @@ def calc_min_max_temperature(x):
     
 def calc_median_temperature(x):
     return stats.median(x)
+
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     numlist = get_user_input()
     average = calc_average_temperature(numlist)
     minmax = calc_min_max_temperature(numlist)
-    median = int(calc_median_temperature(numlist))
+    median = calc_median_temperature(numlist)
     print(f"The average temperature is: {average} degrees celsius")
     print(f"The minimum and maximum temperature are: {minmax} degrees celsius")
     print(f"The median temperature is {median}")
-
 
 if __name__ == "__main__":
     main()
